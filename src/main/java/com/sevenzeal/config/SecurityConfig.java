f258c181ade1e42141606aeb041228368d1f89fd
Vinicius Zanon <vinizanon27@gmail.com>
2026-04-24 17:08:30 -0300

    pqp

--- src/main/java/com/sevenzeal/config/SecurityConfig.java
+++ src/main/java/com/sevenzeal/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 public class SecurityConfig {
@@ -18,5 +19,11 @@ public class SecurityConfig {
             );
 
         return http.build();
+    }
+
+    // 🔥 ESSENCIAL PRO SEU USER SERVICE
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
